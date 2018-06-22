1b1ae1e682220d563b18ecdbdb46c66eda510808
Amy Thompson <amythompson9@gmail.com>
2018-06-22 16:42:57 -0400

    Finished problems

--- UdemyFunctionProblemSet/JavaScript.js
+++ UdemyFunctionProblemSet/JavaScript.js
@@ -1,1 +1,31 @@
-﻿
+﻿//function isEven(num) {
+//    if (num % 2 === 0) {
+//        return true;
+//    }
+//    else {
+//        return false;
+//    }
+//}
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+function factorial(num) {
+    //define a result variable
+    var result = 1;
+    //calculate facotorial and store value in result
+    for (var i = 2; 1 <= num; i++) {
+        result *= i;
+    }
+    //return the result variable
+    return result;
+}
+
+function kebabToSnake(str) {
+    //replace all - with _
+    var newStr = str.replace(/-/g, "_");
+    //return str
+    return newStr;
+}
+
+
